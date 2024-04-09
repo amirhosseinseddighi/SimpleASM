0186c41bb317a13bd3958628d19d9bbab38372f3
Amir Hossein Seddighi <amir30528@gmail.com>
2024-04-09 18:03:10 +0330

    Programs Table added

--- dashboard.php
+++ dashboard.php
@@ -8,7 +8,37 @@ if(!isset($_SESSION['user'])) {
 
 if($_GET['action'] == 'programs') {
 ?>
-        <h1> Programs </h1>
+        <div id="programs_view">
+                <table class="table table-striped table-hover table-condensed small">
+                        <tr>
+                                <th># ID</th>
+                                <th># Program Name</th>
+                                <th># Program Type</th>
+                                <th># Platform</th>
+                                <th># Link</th>
+                                <th># Tags</th>
+                                <th># Actions</th>
+                        </tr>
+                        <?php
+                                // Fetching Programs from database
+                                $stmt = $conn->query("SELECT * FROM `programs`");
+                                $stmt->setFetchMode(PDO::FETCH_ASSOC);
+                                while($row = $stmt->fetch()) {
+                        ?>
+                                        <tr>
+                                                <td># <?php echo $row['id']; ?></td>
+                                                <td><?php echo $row['name']; ?></td>
+                                                <td><?php echo $row['program_type']; ?></td>
+                                                <td><?php echo $row['platform']; ?></td>
+                                                <td><?php echo $row['link']; ?></td>
+                                                <td><?php echo $row['tags']; ?></td>
+                                                <td>ACTIONS</td>
+                                        </tr>
+                        <?php
+                                }
+                        ?>
+                </table>
+        </div>
 <?php
 }
 if($_GET['action'] == 'subs') {
@@ -17,4 +47,9 @@ if($_GET['action'] == 'subs') {
 
 <?php
 } 
+if($_GET['action'] == 'logout') { 
+        session_destroy();
+        header("Location: login.php");
+        exit();
+}
 ?>
--- index.php
+++ index.php
@@ -12,22 +12,28 @@ if(!isset($_SESSION['user'])) {
 <html>
 <head>
     <title>Dashboard</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="src/styles/bootstrap.min.css" rel="stylesheet"/>
     <link href="src/styles/style.css" rel="stylesheet">
 </head>
 <body>
-    <div id="menu">
-        <ul>
-            <li><a href="?action=programs">Programs</a></li>
-            <li><a href="?action=subs">Subs</a></li>
-            <li><a href="?action=logout">Log out</a></li>
-        <ul>
+    <div class="container-fluid">
+        <div id="menu">
+            <ul>
+                <li><a href="?action=programs">Programs</a></li>
+                <li><a href="?action=subs">Subs</a></li>
+                <li><a href="?action=logout">Log out</a></li>
+            <ul>
+        </div>
+        <div id="content">
+            <?php
+                if(isset($_GET['action'])) {
+                    include("dashboard.php");
+                }        
+            ?>
+        </div>
     </div>
-    <div id="content">
-        <?php
-            if(isset($_GET['action'])) {
-                include("dashboard.php");
-            }        
-        ?>
-    </div>
+    <script src="src/scripts/bootstrap.min.js"/>
 </body>
 </html>
